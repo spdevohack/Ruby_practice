bdd3e0b04c8c7be054bc0853f98f51acac7185f6
sachin <sachin@devohack.com>
2021-12-01 11:09:10 +0530

    Create Ruby file

---
+++ Example.rb
@@ -0,0 +1,281 @@
+# puts "Enter Radius Of circle: "
+# r=gets.to_f
+# area=3.14*r*r 
+# paremeter = 2*3.14*r 
+
+# puts "area Of circle: #{area}"
+# puts "paremetr of circle: #{paremeter}"
+
+
+# -------------------------------****************************------------------------------------
+
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# a.each do |num|
+# 	if num >5
+# 		puts num
+# 	end
+
+# end
+ 
+# new_arr = a.select { |number| number % 2 != 0}
+
+# new_arr = a.select do |number|
+# 	number % 2 != 0
+# end
+
+
+# a.unshift(51)
+# puts a
+# a.unshift(23)
+# puts a
+
+# a.uniq!
+# puts a 
+
+# -------------------------------*******************************------------------------------------
+#create Hash
+
+# h ={"a":1, "b":2, "c":3, "d":4}
+
+# hash = {:1 => 'value'}     # Old version of creating Hash
+# hash = {"a" => 1}          # Another  way of creating hash
+
+
+# puts h[:d]                 # Get the value of keys
+
+
+
+# puts h.values             # To get All values of hash
+# puts h.keys    			# To get All keys of hash 
+ 
+# h[:a]= 5         			# Change value of key 
+
+# puts h
+
+
+# hash= {"a"=>1}
+# hash["b"] = 3
+# hash["a"]  = 7
+# hash.delete_if { |key,value| value < 3.5 }                  # 1. hash.delete_if {|k,v| v < 3} Remove Key and Value if condition is given,   2. Remove All key and value from { hash.clear }
+# puts hash
+
+
+
+# -------------------------------*******************************------------------------------------
+
+
+
+
+# name = gets.chop          # (Chomp or chop) is used to remove the new  line cahr in p statement  output  
+# p "name is #{name}"
+
+
+# a = {"sachin" => [ 23, 67, 87326] , "Gunnu" => ["She is Cute"]}     #Hash Value as array
+# puts "#{a}"
+
+
+# b = [{"Sachin" => "coffee"}, {"Satyam" =>"Tea"}, {"Ravinder" => "Milk"}]     #Array of Hashes 
+
+# # puts b[0]                                                            # Retrieve array value by  index 
+
+# puts "Array is : #{b}"
+# puts "#{a ["sachin"]}"
+
+#--------------------------**********************************-------------------------------
+
+# delete all the string start with an 's'
+
+# arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+# arr.delete_if { |str| str.start_with?('s' , 'w') }              #Delete All the string that starts with s 
+
+# puts arr 
+
+  
+#----------------------------*************************************--------------------------
+
+# # Def method 
+
+# name = 'Something Else' 
+
+
+# def full_name(first_name, last_name)
+#     name = first_name + ' '  + last_name
+#     puts "My name is #{name}! "
+# end
+
+
+
+# full_name 'sachin' ,'pal'
+# full_name 'ravinder', 'chouhan'
+# full_name 'satyam', 'sharma'
+
+
+
+
+#---------------------------**************************--------------------------------
+
+# printfirst 10 prime number
+
+# require 'prime'
+
+# puts Prime.take_while(10)
+
+
+
+# a= 7
+
+# 4.times do |n|
+
+# puts a  
+# end
+
+#multiply Two number  by passing in method
+
+# def multiply(a,b)
+# 	return  a*b
+# end
+
+# puts multiply(6,8)
+
+
+
+#for loop in different ways  
+
+# for i in (1...9)
+# 	puts "value is : #{i}"
+# end
+
+
+# (1...9).each do |var| puts "variable is : #{var}"
+# end
+
+
+# for loop with break 
+
+# for i in 0...17 
+# 	if i >7 
+# 		break
+# 	end
+# 	puts "value is : #{i}"
+# end
+
+# def test
+# 	i =9
+# 	j=90
+# 	k=37
+# 	return i, j, k 
+# end
+# var = test
+# puts var
+
+
+# def test
+# 	puts "you are in the method"
+# 	yield  # yield take the parameter from outside that is given with method
+# 	puts "you are again back to method"
+# 	yield
+# end
+
+# test {puts "you are in the block"}
+
+
+# f = 0..9
+# puts f.include?(4)
+# ret =f.reject {|i| i< 4}
+# puts "min value is #{ret}"
+
+# if ((1..10) === 8)    # (=== this check whether it lies in range or not)
+# 	puts "8 lies in (1..10)"
+# end
+
+
+
+#---------------------------*********************************------------------------------
+
+
+
+# Create class
+
+# class Big
+# 	def initialize(l,h)
+# 		@length = l   #@var instance variable are class attributes
+# 		@height = h 
+# 	end
+     
+#     # we can't access these method bcoz these are private so we ahve to use accessor method llike getter and setter
+
+
+#     #Accessor method
+#     def  printlength
+#     	@length
+#     end
+
+#     def printheight
+#     	@height
+#     end
+
+# end
+
+
+# #Create an object
+
+# big = Big.new(19,43)
+
+# #to use accessor methods
+
+# x=big.printheight()
+# y=big.printlength()
+
+# puts "Height is : #{x}"
+# puts "Length is : #{y}"
+
+
+# similarly t0 accessor methods, ruby provides a way to set the values of those variables form outside the calss using sette methods 
+
+
+class Box
+	def initialize(w,h)
+		@width, @height =w,height
+	end
+
+	#accessor method
+
+	def getWidth
+		@width
+	end
+	def getHeight
+		@height
+	end
+
+
+	#setter method 
+
+	def setWidth = (value)
+		@width = value
+	end
+
+	def setHeight = (value)
+		@height = value
+	end
+	
+
+ 	
+
+end
+
+
+box = Box.new(10,20)
+
+
+box.setWidth = 50
+box.setHeight = 70
+
+
+x= box.getWidth()
+y= box.getHeight()
+
+puts "Width is : #{x}"
+
+puts "Height is : #{y}"
